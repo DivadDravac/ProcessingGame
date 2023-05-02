0c7a6e5495489e703cb1bbab0f5d40ed8e9f8ace
DivadDravac <david.cavard@gmail.com>
2023-05-02 10:02:34 +0200

    Créateur de carrtes

--- CardMaker.py
+++ CardMaker.py
@@ -20,19 +20,88 @@ LedR1 = 12
 TableauCartes = []
 im = []
 font = ImageFont.truetype('computer_pixel-7.ttf', 30)   
+fontNb = ImageFont.truetype('computer_pixel-7.ttf', 20)   
 
-def MicroP(Numero):
+
+def MicroP(Numero, fct, activation = 0, IsJoue = False):
     if Numero == -1:
-        CarteImg.paste(im[PicropEx], (184, 75), im[PicropEx])
+        #Prise
         CarteImg.paste(im[Prise8], (176, 0), im[Prise8])
-    elif Numero == 0:
-        CarteImg.paste(im[Prise2], (0, 24), im[Prise2])
-        CarteImg.paste(im[MicropB], (91, 60+66*3), im[MicropB])
+
+        ChipBuf = PrintFctExec(fct)
+
+        CarteImg.paste(ChipBuf, (184,75), ChipBuf)
+        
+
     else:
-        CarteImg.paste(im[Prise4], (0, 58+66*Numero), im[Prise4])
-        CarteImg.paste(im[Chip6], (37, 65+66*Numero), im[Chip6])
-        CarteImg.paste(im[Chip16], (96, 67+66*Numero), im[Chip16])
-    
+        if IsJoue:
+            #Prise
+            CarteImg.paste(im[Prise2], (0, 24), im[Prise2])
+
+            #microchip
+            ChipBuf = PrintFct(fct)
+            CarteImg.paste(ChipBuf[0], (ChipBuf[1][0], ChipBuf[1][1]+ChipBuf[1][2]*Numero), ChipBuf[0])
+
+            #LED
+            CarteImg.paste(im[LedR1], (61, 157+ChipBuf[1][2]*(Numero-1)), im[LedR1])
+
+        else:
+            #Prise
+            CarteImg.paste(im[Prise4], (0, 58+66*Numero), im[Prise4])
+
+            #LED
+            CarteImg.paste(im[LedB1], (40, 166+66*(Numero-1)), im[LedB1])
+
+            #Compteur de marqueur
+            Chip = im[Chip6].copy()
+            carac = ImageDraw.Draw(Chip)                                  
+            carac.text((13,0), str(activation), font=fontNb, fill=(255,255, 255) ) 
+            CarteImg.paste(Chip, (37, 65+66*Numero), Chip)
+
+            #microchip
+            ChipBuf = PrintFct(fct)
+            CarteImg.paste(ChipBuf[0], (ChipBuf[1][0], ChipBuf[1][1]+ChipBuf[1][2]*Numero), ChipBuf[0])
+
+def PrintFctExec(fct):
+    Chip = im[PicropEx].copy()
+    fctList = fct.split("¤")
+    fctList.remove("")
+    carac = ImageDraw.Draw(Chip)  
+
+    if len(fctList)>0:
+        carac.text((120,-5),fctList[0], font=font, fill=(255,255, 255) ) 
+    if len(fctList)>1:
+        carac.text((70,4),fctList[1], font=font, fill=(255,255, 255)) 
+    if len(fctList)>2:
+        carac.text((13,20),fctList[2], font=font, fill=(255,255, 255)) 
+
+    return Chip.rotate(-90, expand=1)
+
+def PrintFct(fct):
+
+    fctList = fct.split("¤")
+    fctList.remove("")
+    Delta = (0,0,0)
+                      
+    if len(fctList) > 1:
+        Chip = im[MicropB].copy()
+        Delta = (91, 62, 65)
+        carac = ImageDraw.Draw(Chip)  
+        i=0
+        for f in fctList:   
+          if f != "":
+            carac.text((10,9+i*20),f, font=font, fill=(255,255, 255) ) 
+            i = i+1
+
+    else:
+        Chip = im[Chip16].copy()
+        carac = ImageDraw.Draw(Chip)  
+        Delta = (96, 69, 65)
+                                        
+        carac.text((10,8),fctList[0], font=font, fill=(255,255, 255) ) 
+
+
+    return (Chip,Delta)
 
 
 #Lit le dossier
@@ -43,7 +112,9 @@ for path in Jeu.FichiersCartes:
     CarteType = path.split("$")[1]
     CarteActivation = int(path.split("$")[2])
     CarteFonction = path.split("$")[3]
-    opencard = Carte.Carte(CarteNom, CarteActivation, CarteFonction, "ComposantsCarte/Circuit.png" , CarteType,0,{})
+    CarteImg = Image.open("ComposantsCarte/Circuit.png")
+    CarteImg.save("test/"+path)
+    opencard = Carte.Carte(CarteNom, CarteActivation, CarteFonction, "test/"+path , CarteType,0,{})
     
     TableauCartes.append(opencard)
     
@@ -58,22 +129,32 @@ for carte in TableauCartes:
     CarteImg = im[0].copy()#Circuit<
     
     if carte.fonctions["Debut"]["Fct"] != '':
-        MicroP(1)
+        MicroP(0, carte.fonctions["Debut"]["Fct"], carte.activation)
     if carte.fonctions["Pioche"]["Fct"] != '':
-        MicroP(2)
+        MicroP(1, carte.fonctions["Pioche"]["Fct"], carte.activation)
     if carte.fonctions["Joue"]["Fct"] != '':
-        MicroP(0)
+
+        if carte.fonctions["Debut"]["Fct"] == "":
+            MicroP(0, carte.fonctions["Joue"]["Fct"],IsJoue=True)
+        elif carte.fonctions["Pioche"]["Fct"] == "":
+            MicroP(1, carte.fonctions["Joue"]["Fct"],IsJoue=True)
+        elif carte.fonctions["Fin"]["Fct"] == "":
+            MicroP(2, carte.fonctions["Joue"]["Fct"],IsJoue=True)
+        elif carte.fonctions["Defausse"]["Fct"] == "":
+            MicroP(3, carte.fonctions["Joue"]["Fct"],IsJoue=True)
+        
+
     if carte.fonctions["Execute"]["Fct"] != '':
-        MicroP(-1)
+        MicroP(-1, carte.fonctions["Execute"]["Fct"])
     if carte.fonctions["Fin"]["Fct"] != '':
-        MicroP(3)
+        MicroP(2, carte.fonctions["Fin"]["Fct"], carte.activation)
     if carte.fonctions["Defausse"]["Fct"] != '':
-        MicroP(4)
+        MicroP(3, carte.fonctions["Defausse"]["Fct"], carte.activation)
 
     CarteImg.paste(im[Nom], (64, 18), im[Nom])
 
     carac = ImageDraw.Draw(CarteImg)                                  
     carac.text((75,27), carte.nom, font=font, fill=(255,255, 255) ) 
 
-    CarteImg.save("test/"+carte.nom+".png")
+    CarteImg.save(carte.imagePath)
 
